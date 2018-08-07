7123f96fcd1a470f3f8a6ba38474325dbc9cbaff
xiaoyingzi <shenyexiaoyingzi@gmail.com>
2018-08-07 15:59:28 +0800

    update

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 /**
  * micro:bit smart car
  */
-//% color="#006400" weight=20 icon="\uf1b9"
+//% color="#006400" weight=10 icon="\uf1b9"
 namespace MicroCar {
 
     const PCA9685_ADD = 0x41
@@ -116,6 +116,30 @@ namespace MicroCar {
         Car_SpinLeft = 6,
         //% blockId="Car_SpinRight" block="Spin Right"
         Car_SpinRight = 7
+    }
+
+    enum remoteButton {
+        Power = 0x0,
+        Up = 0x1,
+        Left = 0x4,
+        Right = 0x6,
+        Down = 0x9,
+        Light = 0x2,
+        BEEP = 0x5,
+        Plus = 0xc,
+        Minus = 0xe,
+        TLeft = 0x8,
+        TRight = 0xA,
+        NUM0 = 0xD,
+        NUM1 = 0x10,
+        NUM2 = 0x11,
+        NUM3 = 0x12,
+        NUM4 = 0x14,
+        NUM5 = 0x15,
+        NUM6 = 0x16,
+        NUM7 = 0x18,
+        NUM8 = 0x19,
+        NUM9 = 0x1A,
     }
 
     function i2cwrite(addr: number, reg: number, value: number) {
@@ -665,5 +689,25 @@ namespace MicroCar {
             case CarState.Car_SpinLeft: Car_spinleft(speed1, speed2); break;
             case CarState.Car_SpinRight: Car_spinright(speed1, speed2); break;
         }
+    }
+
+    /**
+     * 
+     */
+    //% blockId=ir_init
+    //% block="init ir receiver"
+    export function initIR() {
+        IR.init(Pins.P8)
+    }
+
+    /**
+     * 
+     * @param btn 
+     * @param body 
+     */
+    //%blockId=onPressEvent 
+    //% block="on |%btn| button pressed" 
+    export function onPressEvent(btn: RemoteButton, body: () => void): void{
+        IR.onPressEvent(btn, body)
     }
 }
